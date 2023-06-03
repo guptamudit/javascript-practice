37a1b8e08deef1b1daeeb1d763bb36f833767af4
Mudit Gupta <100394697+guptamudit@users.noreply.github.com>
2023-06-03 17:50:45 +0530

    Update index.js

--- DOM/DOM Challenge Starting Files/index.js
+++ DOM/DOM Challenge Starting Files/index.js
@@ -1,1 +1,7 @@
 document.querySelector("ul").lastElementChild.innerHTML = "Mudit";
+document.querySelector("button").style.backgroundColor = "Yellow";
+
+document.querySelector("h1").classList.add("huge");
+
+document.querySelector(".list a").innerHTML = "<em> Google Baba </em>";
+document.querySelector("#google").setAttribute("href", "https://www.bing.com");
