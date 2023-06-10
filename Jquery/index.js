5edb218a54348b42c00b11c8e8058441db049f82
Mudit Gupta <100394697+guptamudit@users.noreply.github.com>
2023-06-10 22:08:49 +0530

    Update index.js

--- Jquery/index.js
+++ Jquery/index.js
@@ -1,4 +1,23 @@
 // $("h1").css("color", "green");
-$("h1").addClass("big-title bg-color length");
+// $("h1").addClass("big-title bg-color length");
+// $("h1").text("Good  Bye");
+$("h1").on("mouseover", function () {
+  setTimeout(() => {
+    $("h1").css("color", "green");
+  }, 350);
+});
+$("h1").on("mouseleave", function () {
+  $("h1").css("color", "yellowgreen");
+});
 
-$("button");
+$("button").click(function () {
+  //   $("h1").toggleClass("font-purple");
+  $("h1").animate({ opacity: 0.5 }, 1000, function () {
+    $(this).css({ opacity: 1 });
+  });
+});
+// console.log($("img").attr("src"));
+console.log($("a").attr("href", "https://yahoo.com"));
+$("input").keypress(function (event) {
+  $("h1").text(event.key);
+});
